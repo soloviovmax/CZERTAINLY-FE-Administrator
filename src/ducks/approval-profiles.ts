4682437c3d7007e3b3b43332cee6798a854ee5e1
soloviovmax <solovyov.amron@gmail.com>
2026-05-21 12:41:16 +0300

    clear approval profile detail when refetching a different version

--- src/ducks/approval-profiles.ts
+++ src/ducks/approval-profiles.ts
@@ -75,7 +75,10 @@ export const slice = createSlice({
         },
 
         getApprovalProfile: (state, action: PayloadAction<{ uuid: string; version?: number }>) => {
-            if (state.profileApprovalDetail?.uuid !== action.payload.uuid) {
+            if (
+                state.profileApprovalDetail?.uuid !== action.payload.uuid ||
+                (action.payload.version !== undefined && state.profileApprovalDetail?.version !== action.payload.version)
+            ) {
                 state.profileApprovalDetail = undefined;
             }
             state.isFetchingDetail = true;
